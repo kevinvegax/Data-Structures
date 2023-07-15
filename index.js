e2988f6861e0d6ddcc1729098f8acffed74c8da5
kevin <ing.kevinvega23@gmail.com>
2023-07-14 19:09:52 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Linked Lists
+
+class LinkedList{
+    constructor(){
+        this.head = null // First element of the List
+        this.tail = null // Last element of the List
+    }
+
+    append(value){
+        const newNode = {value: value}
+    }
+
+}
+
+const linkedList1 = new LinkedList();
