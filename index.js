84d27015473f18e6c6b03fdcd682d48fd121ca12
kevin <ing.kevinvega23@gmail.com>
2023-07-16 17:20:26 -0600

    First change

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 // Linked Lists
 
 class LinkedList{
-    constructor(){
-        this.head = null // First element of the List
-        this.tail = null // Last element of the List
-    }
+  constructor(){
+    this.head = null // First element of the List
+    this.tail = null // Last element of the List
+  }
 
-    append(value){
-        const newNode = {value: value}
-    }
-
+  append(value){
+    const newNode = { value: value, next: null }
+      if(this.tail){
+        this.tail.next = newNode;
+      }
+      this.tail = newNode;
+  }
 }
 
 const linkedList1 = new LinkedList();
